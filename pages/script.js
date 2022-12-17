7128a68db81a654f60dcda5a303c96c742143b3e
Mizerikord <greyguard89@mail.ru>
2022-12-17 15:23:10 +0300

    add changes in html, css, js files, fix miss

--- pages/script.js
+++ pages/script.js
@@ -1,53 +1,35 @@
-let popupActive = document.querySelector('.popup');
+
+
+const popup = document.querySelector('.popup');
 
 //открыть - закрыть попап
 function openPopup(){
-    popupActive.classList.toggle('popup_opened');
+    popup.classList.remove('popup_opened');
+}
+function closePopup(){
+    popup.classList.add('popup_opened');
 }
 document.querySelector('.profile__editor').addEventListener('click', openPopup);
-document.querySelector('.popup__disable').addEventListener('click', openPopup);
+document.querySelector('.popup__disable').addEventListener('click', closePopup);
 
 //редактирвоание полей
-let popupName = document.querySelector('.popup__name');
-let popupTheme = document.querySelector('.popup__theme');
-let profileName = document.querySelector('.profile__name');
-let profileTheme = document.querySelector('.profile__theme');
-let popupButton = document.querySelector('.popup__save-button');
-    //очистка поля
-popupName.addEventListener('click', function(){
-    popupName.value = '';
-})
+const popupName = document.querySelector('.popup__elem_input-name');
+const popupTheme = document.querySelector('.popup__elem_input-theme');
+const profileName = document.querySelector('.profile__name');
+const profileTheme = document.querySelector('.profile__theme');
+const popupForm = document.querySelector('.popup__form')
+const popupButton = document.querySelector('.popup__save-button');
 
-popupTheme.addEventListener('click', function(){
-    popupTheme.value = '';
-})
 //меняем текст на новый
-function newName(){
+
+popupForm.addEventListener('submit',  function newName(event){
+    event.preventDefault();
     if(popupName.value || popupName.value !==''){
     profileName.textContent = popupName.value;
     profileTheme.textContent = popupTheme.value;
-    } else { //оставляем текст, если введено пустое поле
-        alert('Строки не могут быть пустыми, изменения не внесены');
-        profileName.textContent = profileName.textContent;
-        profileTheme.textContent = profileTheme.textContent;
     }
-openPopup();
-}
-
-popupButton.addEventListener('click', newName);
-document.querySelector('.popup').addEventListener('keypress', function(event){
-    if (event.key === 'Enter'){
-       newName();
-    }
+    closePopup();
 });
-
-//ставим Like
-function like(event){
-    if (event.target.classList.contains('card__like')){
-    event.target.classList.toggle('card__dislike');
-    }
-}
-document.querySelector('.card__list').addEventListener('click', like)
 
 //добавляем фоточки разные
 //реализуем в новом сезоне
