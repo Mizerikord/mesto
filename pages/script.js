afab20d3be8893804e04bf71a511676409152d6f
Mizerikord <greyguard89@mail.ru>
2022-12-19 17:48:07 +0300

    third fix mistake

--- pages/script.js
+++ pages/script.js
@@ -5,6 +5,8 @@ const popup = document.querySelector('.popup');
 //открыть - закрыть попап
 function openPopup(){
     popup.classList.remove('popup_opened');
+    popupName.value = profileName.textContent ;
+    popupTheme.value = profileTheme.textContent;
 }
 function closePopup(){
     popup.classList.add('popup_opened');
@@ -13,8 +15,8 @@ document.querySelector('.profile__editor').addEventListener('click', openPopup);
 document.querySelector('.popup__disable').addEventListener('click', closePopup);
 
 //редактирвоание полей
-const popupName = document.querySelector('.popup__elem_input-name');
-const popupTheme = document.querySelector('.popup__elem_input-theme');
+const popupName = document.querySelector('.popup__elem_input_name');
+const popupTheme = document.querySelector('.popup__elem_input_theme');
 const profileName = document.querySelector('.profile__name');
 const profileTheme = document.querySelector('.profile__theme');
 const popupForm = document.querySelector('.popup__form')
@@ -24,7 +26,7 @@ const popupButton = document.querySelector('.popup__save-button');
 
 popupForm.addEventListener('submit',  function newName(event){
     event.preventDefault();
-    if(popupName.value || popupName.value !==''){
+    if(popupName.value || popupTheme.value !==''){
     profileName.textContent = popupName.value;
     profileTheme.textContent = popupTheme.value;
     }
